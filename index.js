5912af3cfcb1220c50502d960e9d07af0a0ef7e8
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-09-03 00:13:20 +0600

    add arry in local storage chnage

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// add object in local storage...................
 const myObject = {
     name: "shuvo",
     age: 24,
@@ -9,3 +10,11 @@ localStorage.setItem("myObject", objectString);
 localStorage.getItem("myObject");
 const parseObject = JSON.parse(objectString)
 console.log(parseObject.name)
+
+
+// Add array in local storage................
+const myArray = [1, 2, 3, 4, 5, 6, 7];
+const arrayString = JSON.stringify(myArray)
+localStorage.setItem('myArray', arrayString)
+localStorage.getItem('myArray')
+const parseArray = JSON.parse(arrayString)
